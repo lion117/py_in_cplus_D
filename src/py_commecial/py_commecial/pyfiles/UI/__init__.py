4cfe5efb578e6da97e91725fe1a35a9a06231b9e
lion117 <ngixpro@qq.com>
2016-10-22 13:08:51 +0830

    remove useless project

--- src/py_commecial/py_commecial/pyfiles/UI/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 01 16:22:39 2014
-
-@author: LEO
-"""
-
--- src/py_formal_package_demo/dlltest/python/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 29 15:12:12 2014
-
-@author: LEO
-"""
-
